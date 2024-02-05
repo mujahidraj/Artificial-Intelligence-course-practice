d6aee7409a7d2df2426ce0228e5550743d94c6e7
Mujahid Raj <113965511+mujahidraj@users.noreply.github.com>
2024-02-06 00:53:28 +0600

    Add files via upload

---
+++ first.py
@@ -0,0 +1,5 @@
+print("Hello")
+print("MOIZUDDIN MOHAMMAD MUJAHID RASHID RAJ")
+print("American International University-Bangladesh")
+print("Hello everyone! Hope you guys are fine.")
+print("ID : 22-46398-1")
